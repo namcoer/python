8534b5a35298d98c884871cc4da93dd81e9881be
namcoer <90943218+namcoer@users.noreply.github.com>
2021-10-02 10:59:51 +0700

    Add files via upload

---
+++ python/vd2.py
@@ -0,0 +1,9 @@
+nhập panda làm pd
+
+series_ex = {"a":[1, 4, 6, 7, 8], "b":[9, 10, 11, 12, 13], "c":[14, 15, 16, 17, 18]}
+
+series_data = pd. DataFrame(series_ex)
+
+in(series_data)
+print("Xuat ra cot dau cua series")
+in(series_data. iloc[:, 0])
---
+++ python/vd2sua.py
@@ -0,0 +1,11 @@
+nhập panda làm pd
+
+dataframe_ex = {"a":[1, 4, 6, 7, 8], "b":[23, 12, 56, 8, 22], "c":[87, 65, 43, 11, 34]}
+
+dataFrame_data = pd. DataFrame(dataframe_ex)
+
+in(dataFrame_data)
+
+print("Xuat ra cot dau cua series")
+
+in(dataFrame_data. iloc[:, 0])
