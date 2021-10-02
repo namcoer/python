f6cbb6ff33a3744ca3976d7fb73cfbd9eeb921c1
namcoer <90943218+namcoer@users.noreply.github.com>
2021-10-02 11:01:49 +0700

    Add files via upload

---
+++ python/baitap.py
@@ -0,0 +1,5 @@
+nhập panda làm pd
+data = {"a":pd. Series([3,48,66,58774],index=[1,2,3,4]),"b": pd. Series([5848,9887,4543,43345],index=[1,2,3,4])}
+df = pd. DataFrame(dữ liệu)
+print('in cột đầu của series:')
+in(df["a"])
