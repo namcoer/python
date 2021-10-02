8a752b33ee67823e0cb9d4cecd2f3e15615c9384
namcoer <90943218+namcoer@users.noreply.github.com>
2021-10-02 11:25:31 +0700

    Add files via upload

---
+++ diemdanh/btdd.py
@@ -0,0 +1,9 @@
+nhập khẩu gấu trúc làm gấu trúc
+
+series_ex = {"a":[1, 3, 5, 7,9], "b":[10, 11, 12, 13, 14],"c":[15, 16, 17, 18, 19]}
+
+series_data = pd. DataFrame(series_ex)
+
+in(series_data)
+print("Xuat ra cot dau cua series)
+in(series_data. iloc[:, 0])
